2917bb4c595f005e59920037b77c9e5c53a5eba3
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-03-22 16:27:41 +0100

    Started the first lecture

---
+++ 01-intro.R
@@ -0,0 +1,106 @@
+#
+# 01-intro.R
+#
+
+#
+# Help! I need somebody, Help...! -- Beatles
+#
+# Listen that song? 
+# 1. Remove the comment symbol '#' (pound sign)
+# 2. Run the function 'browseURL()'
+# browseURL("https://www.youtube.com/watch?v=2Q_ZzBGPdqE")
+
+#
+# What did we already learn about R?
+# 
+# 1. Comment things using the pound sign ('#') -- vice versa!
+# 2. Functions are always suffixed by parantheses '()' 
+
+#
+# What does browseURL do? You experienced it! But that's not always possible,
+# so we need learn about help functions.
+#
+
+# help() & '?'
+#
+
+# Help on help!
+help("?")
+
+# Help on a function
+help("browseURL")
+
+# TODO: Compare! How do they differ? 
+help("browseURL")
+?browseURL
+
+# Get help on special symbols
+help("?") 
+?"?"
+?help
+
+# Hold on, each function needs '()', don't they?
+?("?")
+?("browseURL")
+
+# TODO: Which one do you prefere, why? 
+# Discuss with you neighbor!
+# t: 2 min
+
+# Take home box!
+#######################################################
+#                                                     #
+# TODO: Document your facourite search snippet below! #
+#                                                     #
+#######################################################
+
+# help.search() & '??' 
+#
+
+
+
+
+
+
+# Take home box!
+#######################################################
+#                                                     #
+# TODO: Document your facourite search snippet below! #
+#                                                     #
+#######################################################
+
+# help.search
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# Homework
+#
+
+# TODO: What do we mean by saying "R is case sensitve"? Give two examples.
+
+
+# TODO: How can case sensitivity bias your search query? Compare!
+?browseURL
+?BrowseURL
+
+# TODO: What does the following function?
+?RSiteSearch
+
+# TODO: Start an online search for browseURL()
+# Check if you did choose:
+# https://search.r-project.org/R/refmans/utils/html/browseURL.html
+
+
