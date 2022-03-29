36b039ac6384eb648980e5e3a48e0ef072dd9fd4
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2022-03-29 18:08:52 +0200

    Started the second script

---
+++ 02-idk.R
@@ -0,0 +1,94 @@
+#
+# Get information about ...everything!
+#
+Sys.locale()
+
+
+#
+# R? A calculator?
+#
+
+
+
+
+
+#
+# Vectors!
+#
+
+# TODO: google! What is a vector in R? (not in general!)
+
+
+# Numeric vector
+#
+x <- 1
+x
+
+
+
+# What does c() do?
+
+# Workflow
+# 1. Check man page, examples, vignettes
+?c
+example("c")
+
+# Happy? No?
+# 2. Check google!
+
+
+# Let's try it!
+X <- c(1,2,3)
+X
+
+
+#
+# Trick: the "further assignment" using c()
+#
+y <- c(X,X)
+y
+
+
+# Character vector
+#
+a <- "Hihihihi"
+a
+
+X <- c("I", "am", "a", "Nerd")
+X
+
+# Trick: unpack the invisible assignment!
+(a <- "Hihihihi")
+
+# ...instead of
+a <- "Hihihihi"
+a
+
+
+
+
+
+
+> source("commands.R")
+For Windows Source is also available on the File menu. The function sink,
+> sink("record.lis")
+will divert all subsequent output from the console to an external file, record.lis. The command
+> sink()
+restores it to the console once again.
+
+
+#
+# Asking questions on stackoverflow
+#
+
+
+
+
+
+
+
+
+
+
+
+
